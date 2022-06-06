52dae3e8a2840a42871f54fb6cea09531508ebcb
DipaLand Center <106227803+DipaLand@users.noreply.github.com>
2022-06-06 08:25:02 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
-global.botname = 'Cheems Bot MD V2'
-global.footer = 'Xeon Bot Inc.'
+global.owner = ['6281381685954']
+global.premium = ['6281381685954']
+global.ownernomer = '6281381685954'
+global.ownername = 'MrclNotWibu?'
+global.botname = 'MrclL-MD'
+global.footer = 'MrclNotWibu'
 global.ig = 'https://github.com/DGXeon'
 global.region = 'India, NorthEast, Mizoram'
-global.sc = 'https://github.com/DGXeon/CheemsBot-MD2'
-global.myweb = 'https://youtu.be/imFIX-Wrt3s'
-global.packname = 'Cheems Bot'
-global.author = '🦄Dream Guy Xeon'
+global.sc = 'https://github.com/DipaLand/hehe'
+global.myweb = 'https://github.com/DipaLand'
+global.packname = 'Yang Nyolong'
+global.author = 'Pantatnya Rapet\nBY: MrclNotWibu'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
